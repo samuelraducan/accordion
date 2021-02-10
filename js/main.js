d57255c39c29c34439b59f51968401e1792f5e43
Samuel Raducan <samuel.raducann@gmail.com>
2021-02-10 15:05:25 +0000

    Added keyboard interaction. Escape & Arrow Keys.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,97 @@
+// =====================================
 /* NOTE: Global Variables */
 const accordionContainer = document.querySelector('.accordion-container');
 
+// =====================================
 /* NOTE: Functions */
 
+/**
+ * Checks if an accordion is opened
+ * @param {HTMLElement} accordion
+ */
+const isAccordionOpen = accordion => {
+  return accordion.classList.contains('is-open');
+};
+
+/**
+ * Opens an accordion
+ * @param {HTMLElement} accordion
+ */
+const openAccordion = accordion => {
+  const accordionContent = accordion.querySelector('.accordion__content');
+
+  accordionContent.style.height = `${getContentHeight(accordion)}px`;
+  accordion.classList.add('is-open');
+};
+
+/**
+ * Closes an accordion
+ * @param {HTMLElement} accordion
+ */
+const closeAccordion = accordion => {
+  const accordionHeaderButton = accordion.querySelector('button');
+  const accordionContent = accordion.querySelector('.accordion__content');
+
+  accordion.classList.remove('is-open');
+  accordionContent.style.height = 0;
+  accordionHeaderButton.focus();
+};
+
+/**
+ * Finds the height of an accordion's content
+ * @param {HTMLElement} accordion The accordion
+ * @returns {Number} Accordion content's height in px value
+ */
+const getContentHeight = accordion => {
+  const accordionInner = accordion.querySelector('.accordion__inner');
+  return accordionInner.getBoundingClientRect().height;
+};
+
+// ======================================
 /* NOTE: Event Listeners */
+
+// Open/Close accordion when user clicks on an accordion's header.
 accordionContainer.addEventListener('click', event => {
-  // Event Delegation.
   const accordionHeader = event.target.closest('.accordion__header');
+  if (!accordionHeader) return;
 
-  if (accordionHeader) {
-    const accordion = accordionHeader.parentElement;
-    const accordionContent = accordionHeader.nextElementSibling;
-    const accordionInner = accordionContent.children[0];
+  const accordion = accordionHeader.parentElement;
 
-    let height = 0;
-    if (accordion.classList.contains('is-open')) {
-      hight = 0;
-    } else {
-      height = accordionInner.getBoundingClientRect().height;
-    }
+  isAccordionOpen(accordion)
+    ? closeAccordion(accordion)
+    : openAccordion(accordion);
+});
 
-    accordion.classList.toggle('is-open');
-    accordionContent.style.height = height + 'px';
+// Close accordion with Escape key.
+document.addEventListener('keydown', event => {
+  const accordion = event.target.closest('.accordion');
+
+  if (event.key !== 'Escape') return;
+  if (!accordion) return;
+
+  if (isAccordionOpen(accordion)) {
+    closeAccordion(accordion);
+  }
+});
+
+// Switch accordions with up/down arrow keys.
+document.addEventListener('keydown', event => {
+  if (!event.target.closest('.accordion__header')) return;
+
+  const key = event.key;
+  const accordion = event.target.closest('.accordion');
+  const accordions = [...accordionContainer.querySelectorAll('.accordion')];
+  const index = accordions.findIndex(element => element === accordion);
+
+  let targetAccordion;
+
+  if (key === 'ArrowDown') {
+    targetAccordion = accordions[index + 1];
+  } else if (key === 'ArrowUp') {
+    targetAccordion = accordions[index - 1];
+  }
+
+  if (targetAccordion) {
+    targetAccordion.querySelector('button').focus();
   }
 });
